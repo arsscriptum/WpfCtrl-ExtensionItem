480f479315e940b4650a81598f8ce301ab76b63c
Guillaume Plante <guillaume.plante@icloud.com>
2025-08-24 19:02:35 -0400

    auto

---
+++ src/GridConfigPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WebExtensionPack.Controls
+{
+    /// <summary>
+    /// Interaction logic for GridConfigPagePageControl.xaml
+    /// </summary>
+    public partial class GridConfigPagePageControl : UserControl
+    {
+        /// <summary>
+        /// A handle to the Settings instance that this control is bound to.
+        /// </summary>
+        private GridConfigSettings _settings = null;
+
+        public GridConfigPagePageControl(GridConfigPage settings)
+        {
+           // InitializeComponent();
+           // _settings = settings;
+            this.DataContext = _settings;
+        }
+
+        private void btnRestoreDefaultSettings_Click(object sender, RoutedEventArgs e)
+        {
+           // _settings.ResetSettings();
+        }
+
+        private void UserControl_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            // Find all TextBoxes in this control force the Text bindings to fire to make sure all changes have been saved.
+            // This is required because if the user changes some text, then clicks on the Options Window's OK button, it closes
+            // the window before the TextBox's Text bindings fire, so the new value will not be saved.
+           /* foreach (var textBox in DiffAllFilesHelper.FindVisualChildren<TextBox>(sender as UserControl))
+            {
+                var bindingExpression = textBox.GetBindingExpression(TextBox.TextProperty);
+                if (bindingExpression != null) bindingExpression.UpdateSource();
+            }*/
+        }
+    }
+}
---
+++ src/Settings.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace WebExtensionPack.Controls
+{
+  // Special guid to tell it that this is a custom Options dialog page, not the built-in grid dialog page.
+    public class GridConfigSettings : INotifyPropertyChanged
+    {
+        #region Notify Property Changed
+        /// <summary>
+        /// Inherited event from INotifyPropertyChanged.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Fires the PropertyChanged event of INotifyPropertyChanged with the given property name.
+        /// </summary>
+        /// <param name="propertyName">The name of the property to fire the event against</param>
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        /// <summary>
+        /// Get / Set if new files being added to source control should be compared.
+        /// </summary>
+        public bool CompareNewFiles { get { return _compareNewFiles; } set { _compareNewFiles = value; NotifyPropertyChanged("CompareNewFiles"); } }
+        private bool _compareNewFiles = false;
+
+        #region Overridden Functions
+
+        /// <summary>
+        /// Gets the Windows Presentation Foundation (WPF) child element to be hosted inside the Options dialog page.
+        /// </summary>
+        /// <returns>The WPF child element.</returns>
+
+
+        /// <summary>
+        /// Should be overridden to reset settings to their default values.
+        /// </summary>
+
+        #endregion
+    }
+}
