623fd6346ad8941e508785fb874ea1eaed53ad52
guillaume plante <guillaumeplante.qc@gmail.com>
2025-09-05 13:32:06 -0400

    Update

---
+++ scripts/Common.ps1
@@ -0,0 +1,240 @@
+﻿
+
+
+try{
+        function Get-ProjectRootPath {
+            [CmdletBinding(SupportsShouldProcess)]
+            param()
+            process {
+                $ProjectRootPath = (Resolve-Path -Path "$PSScriptRoot\..").Path
+                return $ProjectRootPath
+            }
+        }
+
+        function Get-SourcesPath {
+            [CmdletBinding(SupportsShouldProcess)]
+            param()
+            process {
+                $SourcesPath = Join-Path (Get-ProjectRootPath) "src"
+                return $SourcesPath
+            }
+        }
+
+
+        function Get-BinariesPath {
+            [CmdletBinding(SupportsShouldProcess)]
+            param()
+
+            $BinariesPath = Join-Path (Get-SourcesPath) "bin"
+            return $BinariesPath
+        }
+
+        function Get-ProjectFrameworkVersion {
+            [CmdletBinding(SupportsShouldProcess)]
+            param()
+            process {
+                $SourcesPath = "net472"
+                return $SourcesPath
+            }
+        }
+
+        $DebugPath = Join-Path (Get-BinariesPath) "Debug"
+
+        $ReleasePath = Join-Path (Get-BinariesPath) "Release"
+        $DeployedRootPath = Join-Path (Get-ProjectRootPath) "libs"
+
+
+[pscustomobject]$Script:ProjectSettingsToSave = [pscustomobject]@{
+        ProjectRootPath = Get-ProjectRootPath
+        SourcesPath = Join-Path (Get-ProjectRootPath) "src"
+        ScriptsPath = Join-Path (Get-ProjectRootPath) "scripts"
+        BinariesPath = Join-Path (Get-SourcesPath) "bin"
+        ObjectsPath = Join-Path (Get-SourcesPath) "obj"
+        FrameworkVersion = "net472"
+        DeployedRootPath = "$DeployedRootPath"
+
+        BinariesDebugPath = Join-Path $DebugPath (Get-ProjectFrameworkVersion)
+        BinariesReleasePath = Join-Path $ReleasePath (Get-ProjectFrameworkVersion)
+        DeployedAssembliesPath = "$($DeployedRootPath)\%Target%"
+        
+
+    }
+    }catch{
+        
+    }
+
+
+function Write-ProjectPathProperties {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory = $true, Position = 0, HelpMessage = "Name of the scheduled task")]
+        [string]$ProjectName,
+
+        [Parameter(Mandatory = $true, Position = 1, HelpMessage = "Properties of the scheduled task")]
+        [pscustomobject]$ProjectProperties
+    )
+
+    $registryPath = "HKCU:\SOFTWARE\arsscriptum\development\projects\{0}\paths" -f $ProjectName
+    $registryPathTypes = "$registryPath\Types"
+
+    # Ensure the registry path exists
+    if (-not (Test-Path $registryPath)) {
+        New-Item -Path $registryPath -Force | Out-Null
+    }
+    if (-not (Test-Path $registryPathTypes)) {
+        New-Item -Path $registryPathTypes -Force | Out-Null
+    }
+
+    foreach ($prop in $ProjectProperties.PSObject.Properties) {
+        $VariableName = "$($prop.Name)"
+        $value = $prop.Value
+        $type = $value.GetType()
+        $VariableValue = ($prop.Value -as $prop.TypeNameOfValue)
+        $VariableType = ($prop.Value).GetType()
+        $VariableTypeFull = $VariableType.FullName
+        $RegValueType = 'String' # Default
+
+        Write-Verbose "$VariableName is a [$($VariableType.Name)] ($VariableTypeFull)"
+
+        $DefaultType = $False
+
+        if (($VariableType -eq [uint32]) -or ($VariableType -eq [int32])) {
+            $RegValueType = 'DWord'
+        } elseif (($VariableType -eq [bool]) -or ($VariableType -eq [Boolean])) {
+            $RegValueType = 'Binary'
+        } elseif (($VariableType -eq [decimal]) -or ($VariableType -eq [int64]) -or ($VariableType -eq [uint64])) {
+            $RegValueType = 'QWord'
+        } elseif ($VariableType -eq [string[]]) {
+            $RegValueType = 'MultiString'
+        } elseif ($VariableType -eq [string]) {
+            $RegValueType = if ($VariableValue -match '[$%]') { 'ExpandString' } else { 'String' }
+        } else {
+            $DefaultType = $True
+            $RegValueType = 'String'
+        }
+
+        if ($DefaultType) {
+            Write-Verbose "cannot identify $VariableName registry type. default to string"
+        } else {
+            Write-Verbose "identified $VariableName registry type to $RegValueType"
+        }
+        Write-Verbose "Property `"$VariableName`" has value $VariableValue as [$VariableType] ($VariableTypeFull). Saved as $RegValueType"
+        # Write to registry
+        try {
+            # registry value type -> $Kind
+            #  "String", "ExpandString", "Binary", "DWord", "MultiString", "QWord"
+            Write-Verbose "[Write-Sched$ProjectProperties] New-ItemProperty -Path $registryPath -Name $VariableName -Value $VariableValue -PropertyType $RegValueType"
+
+            New-ItemProperty -Path $registryPath -Name $VariableName -Value $VariableValue -PropertyType $RegValueType -Force | Out-null
+            New-ItemProperty -Path $registryPathTypes -Name "$VariableName" -Value "$VariableTypeFull" -PropertyType 'String' -Force | Out-null
+        } catch {
+            Write-Warning "Failed to register '$property_name': $_"
+        }
+    }
+}
+
+
+
+function Read-ProjectPathProperties {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0, HelpMessage = "Name of the scheduled task")]
+        [string]$ProjectName
+    )
+
+    process {
+
+        $registryPath = "HKCU:\SOFTWARE\arsscriptum\development\projects\{0}\paths" -f $ProjectName
+        $registryPathTypes = "$registryPath\Types"
+
+        if (-not (Test-Path $registryPath)) {
+            throw "Task registry path '$registryPath' does not exist."
+        }
+
+
+        $Key = Get-Item -Path $registryPath -ErrorAction Stop
+        $Properties = $Key.Property
+
+        foreach ($property_name in $Properties) {
+            try {
+                # Read raw string type from Types subkey
+                $typeString = (Get-ItemProperty -Path $registryPathTypes -Name $property_name).$property_name
+                $property_value = (Get-ItemProperty -Path $registryPath -Name $property_name).$property_name
+
+                # Convert type string to actual [type]
+                $resolvedType = [type]::GetType($typeString, $false)
+
+                if ($null -eq $resolvedType) {
+                    Write-Warning "Could not resolve type '$typeString' for property '$property_name'. Defaulting to string."
+                    $resolvedType = [string]
+                }
+
+                # Try casting the value to the original type
+                if (($resolvedType -eq [bool]) -or ($resolvedType -eq [Boolean])) {
+                    $converted_boolean = if ($property_value -eq '0') { $False } else { $True }
+                    $convertedValue = [bool]::Parse($converted_boolean)
+                }
+                elseif ($resolvedType.IsEnum) {
+                    $convertedValue = [Enum]::Parse($resolvedType, $property_value)
+                }
+                elseif ($resolvedType -eq [string[]] -and ($property_value -is [string])) {
+                    $convertedValue =, $property_value # Ensure it's an array
+                }
+                else {
+                    $convertedValue = $property_value -as $resolvedType
+                }
+
+                $LogPart1 = "Registering new global variable `"{0}`"" -f "$property_name"
+                $LogPart2 = "representing the path `"{0}`"" -f "$convertedValue"
+                $LogFull = "   ✔   {0,-60} {1,-80}" -f "$LogPart1", "$LogPart2"
+
+                
+                try {
+                    New-Variable -Name "$property_name" -Value "$convertedValue" -Option AllScope -Visibility Public -Force -Scope Global -Confirm:$false -ErrorAction Ignore
+                   Write-Host "$LogFull" -f White
+                }catch{
+
+                    Write-Host "   ⚠️ variable `"$property_name`" already registered" -f White
+                }
+                
+            } catch {
+                Write-Warning "Failed to restore property '$property_name': $_"
+            }
+        }
+
+        return $ProjectProperties
+    }
+}
+
+
+
+
+function Reset-RegistryProjectPathProperties {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string]$ProjectName = "ExtensionItemCtrl"
+    )
+    try{
+
+    Write-ProjectPathProperties -ProjectName "$ProjectName" $Script:ProjectSettingsToSave 
+    }catch{
+        Write-Error "$_"
+    }
+
+
+}
+
+
+
+function Initialize-RegistryProjectPathProperties {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string]$ProjectName = "ExtensionItemCtrl"
+    )
+
+
+    Read-ProjectPathProperties -ProjectName "$ProjectName" 
+
+}
---
+++ scripts/Include.ps1
@@ -0,0 +1,274 @@
+
+
+function Import-BuildSystemScripts {
+    [CmdletBinding(SupportsShouldProcess)]
+    param()
+    $regPath = "HKCU:\SOFTWARE\arsscriptum\development\powershell\buildsystem"
+    $scriptsPath = (Get-ItemProperty -Path $regPath -Name "ScriptsPath").ScriptsPath
+    if (-not (Test-Path $scriptsPath -PathType Container)) {
+        throw "ScriptsPath '$scriptsPath' does not exist."
+    }
+    Get-ChildItem -Path $scriptsPath -Filter '*.ps1' | ForEach-Object {
+        $fn = "$($_.FullName)"
+        Write-Verbose "$fn"
+        . "$fn"
+    }
+}
+
+
+
+
+function Find-LoadedModule {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, HelpMessage="DLL or module name (e.g., WebExtensionPack.Controls.dll)")]
+        [string]$Module,
+        [Parameter(Mandatory=$false)]
+        [string]$ProcessName,
+        [Parameter(Mandatory=$false)]
+        [switch]$Kill
+    )
+
+    $tasklistExe = (Get-Command "tasklist").Source
+    $results = &"$tasklistExe" '/m' '/FO' 'CSV'
+    $allProcesses = $results | ConvertFrom-Csv
+    $allProcessesFiltered = $allProcesses | sort -Property PID -Descending
+    $allProcessesCount = $allProcesses.Count
+    Write-Host "$allProcessesFilteredCount Processes to search."
+    if(-not([string]::IsNullOrEmpty($ProcessName))){
+        $allProcessesFiltered = $allProcesses.Where({$_.'Image Name' -eq "$ProcessName"})  | sort -Property PID -Descending
+        $allProcessesFilteredCount = $allProcessesFiltered.Count
+        Write-Verbose "Filetering Process list with `"$ProcessName`". $allProcessesFilteredCount Processes to search ($allProcessesCount => $allProcessesFilteredCount)"
+    }
+    [system.Collections.ArrayList]$ResultsList = [System.Collections.ArrayList]::new()
+    [string[]]$WhenIncluded = @()
+    [string[]]$NotIncluded = @()
+
+    ForEach($ps in $allProcessesFiltered){
+        [int]$ProcessId = $ps.PID
+        [string]$ProcessName = $ps.'Image Name'
+        
+        [string[]]$ModulesList = $ps.Modules.Split(',')
+        $ModulesListCount = $ModulesList.Count
+        $ModuleFound = $ModulesList.Contains($Module)
+        if($ModuleFound){
+            [PsCustomObject]$pobj = [PsCustomObject]@{
+                ProcessName = $ProcessName
+                ProcessId = $ProcessId
+                FoundModule = $Module
+                ModulesList = $ModulesList
+            }
+            [void]$ResultsList.Add($pobj )
+            Write-Verbose " ✔ Process $ProcessName (pid $ProcessId) loaded $ModulesListCount modules. Out of which $Module was included"
+            $WhenIncluded = $ModulesList
+            if($($WhenIncluded.Count) -gt $($NotIncluded.Count)){
+                $Diff = $WhenIncluded | Where-Object { $_ -notin $NotIncluded } 
+                $diffStr = $diff -join ", "
+                Write-Verbose "Difference: $diffStr"
+            }
+        }
+        else{
+            Write-Verbose " ❌ Process $ProcessName (pid $ProcessId) loaded $ModulesListCount modules, but $Module was not included"
+            $NotIncluded = $ModulesList  
+        }
+    }   
+
+
+    if($Kill){
+        ForEach($r in $ResultsList){
+            Write-Verbose "Killing pid $($r.ProcessId)"
+            Get-Process -Id  $($r.ProcessId) | Stop-Process -Force -Confirm:$False
+        }
+    }
+
+    $ResultsList
+}
+
+
+function Get-ProcessUsingModule {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, HelpMessage="DLL or module name (e.g., WebExtensionPack.Controls.dll)")]
+        [string]$Module,
+        [Parameter(Mandatory=$false)]
+        [switch]$Kill
+    )
+
+    $tasklistExe = (Get-Command "tasklist").Source
+
+    $result = &"$tasklistExe" '/m' "$Module" '/FO' 'CSV'
+    $found = $result | Where-Object { $_.'Modules' -match [regex]::Escape($Module) }
+    if( ($result -match 'No tasks') -Or (-not $found)) {
+        Write-Host "No process found using module: $Module" -ForegroundColor Yellow
+        return @()
+    }
+
+    $found | ForEach-Object {
+        Write-Host "Image Name: $($_.'Image Name'), PID: $($_.PID), Modules: $($_.Modules)"
+        if ($Kill) {
+            try {
+                Stop-Process -Id ([int]$_.PID) -Force -ErrorAction Stop
+                Write-Host "Killed PID $($_.PID)" -ForegroundColor Red
+            } catch {
+                Write-Host "Failed to kill PID $($_.PID): $_" -ForegroundColor DarkRed
+            }
+        }
+    }
+
+    return $found
+}
+
+
+
+function Out-BuildTitle {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipeline = $True)]
+        [string]$Title,
+        [Parameter(Mandatory = $false)]
+        [switch]$Red
+    )
+
+    process {
+        $Len = 120
+        $TitleLen = $Title.Length
+        $LineSep = [string]::new('=', $Len)
+        $SpacesLen = ($Len / 2) - ($TitleLen / 2)
+        $Spaces = [string]::new(' ', $SpacesLen)
+        $TitleStr = "{0}{1}{0}" -f $Spaces, $Title
+        if ($Red) {
+            Write-Host "$LineSep" -f DarkRed
+            Write-Host "$TitleStr" -f DarkYellow
+            Write-Host "$LineSep" -f DarkRed
+        } else {
+            Write-Host "$LineSep" -f DarkCyan
+            Write-Host "$TitleStr" -f White
+            Write-Host "$LineSep" -f DarkCyan
+        }
+
+    }
+
+}
+
+function Invoke-BuildRequest {
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [Parameter(Position = 0, Mandatory = $true, HelpMessage = "BuildRequest object")]
+        [BuildRequest]$BuildRequest = $Null,
+        [Parameter(Mandatory = $false)]
+        [switch]$Clean
+    )
+
+    $regPath = "HKCU:\SOFTWARE\arsscriptum\development\powershell\buildsystem"
+    $dotnetPath = (Get-ItemProperty -Path $regPath -Name "DotNetPath").DotNetPath
+
+    if (-not $BuildRequest) {
+        $RequestsRemaining = Test-BuildRequestsRemaining
+        if ($RequestsRemaining -eq 0) {
+            Write-Host "No build request found in queue." -ForegroundColor Yellow
+            return
+        }
+        $BuildRequest = Get-NextBuildRequest
+    }
+
+    $OutPath = Join-Path "$($BuildRequest.WorkingDirectory)" "$($BuildRequest.OutputPath)"
+    $OutPath = Join-Path "$OutPath" "$($BuildRequest.Configuration)"
+    $RealDeployPath = Join-Path "$($BuildRequest.DeployPath)" "$($BuildRequest.Configuration)"
+
+    Write-Host "Processing BuildRequest ID $($BuildRequest.BuildId) GUID $($BuildRequest.GUID)" -ForegroundColor Cyan
+    Write-Host "Working Directory: $($BuildRequest.WorkingDirectory)"
+    Write-Host "Project File: $($BuildRequest.ProjectFilePath)"
+    Write-Host "Architecture: $($BuildRequest.Architecture)"
+    Write-Host "Output Path: $($BuildRequest.OutputPath)" -f DarkCyan
+    Write-Host "Deploy Path: $RealDeployPath" -f DarkRed
+    Write-Host "Configuration: $($BuildRequest.Configuration)" -f DarkRed
+    Write-Host "Framework: $($BuildRequest.Framework)"
+    Write-Host "Version: $($BuildRequest.Version)"
+    Write-Host "Log Level: $($BuildRequest.LogLevel)"
+    Write-Host "Compilation Output Path: $OutPath" -f DarkCyan
+    $MsBuildPropertiesCount = $BuildRequest.MsBuildProperties.Count
+    if ($MsBuildPropertiesCount) {
+        Write-Host "MsBuild Properties"
+        Write-Host " - $($BuildRequest.MsBuildProperties | ForEach-Object { "$($_.Key)=$($_.Value)" } -join '; ')"
+    }
+
+
+    # Clean output path if requested
+    if ($Clean -and $OutPath -and (Test-Path $OutPath)) {
+        Write-Host "Cleaning output path: $($OutPath)" -ForegroundColor Yellow
+        Remove-Item -Path $OutPath -Recurse -Force -ErrorAction SilentlyContinue
+    }
+
+    $args = @('build', "$($BuildRequest.ProjectFilePath)")
+    if ($BuildRequest.Architecture) { $args += @('--arch', $BuildRequest.Architecture) }
+    if ($BuildRequest.ArtifactsPath) { $args += @('--artifacts-path', $BuildRequest.ArtifactsPath) }
+    if ($BuildRequest.Configuration) { $args += @('-c', $BuildRequest.Configuration) }
+    if ($BuildRequest.Framework) { $args += @('-f', $BuildRequest.Framework) }
+    if ($OutPath) { $args += @('-o', $OutPath) }
+    if ($BuildRequest.Version) { $args += @('-p:Version=' + $BuildRequest.Version) }
+    if ($BuildRequest.MsBuildProperties.Count -gt 0) {
+        $propList = $BuildRequest.MsBuildProperties | ForEach-Object { "$($_.Key)=$($_.Value)" }
+        $args += @("-p:" + ($propList -join ";"))
+    }
+    if ($BuildRequest.LogLevel) {
+        $args += @('-v', "$($BuildRequest.LogLevel)")
+    }
+    if (-not $BuildRequest.Incremental) {
+        $args += '--no-incremental'
+    }
+
+    Write-Host "dotnet command: $dotnetPath" -ForegroundColor Magenta
+    Write-Host "dotnet arguments:" -ForegroundColor Magenta
+    $args | ForEach-Object { Write-Host "  $_" -ForegroundColor Magenta }
+
+    $processParams = @{
+        FilePath = $dotnetPath
+        ArgumentList = $args
+        WorkingDirectory = $BuildRequest.WorkingDirectory
+        NoNewWindow = $true
+        PassThru = $true
+    }
+    if ($BuildRequest.RedirectStdOut) { $processParams['RedirectStandardOutput'] = (New-TemporaryFile).FullName }
+    if ($BuildRequest.RedirectStdErr) { $processParams['RedirectStandardError'] = (New-TemporaryFile).FullName }
+
+    $BuildRequest.Executed = [uint64]([math]::Floor((Get-Date).ToUniversalTime().Subtract([datetime]'1970-01-01').TotalSeconds))
+    $proc = Start-Process @processParams
+    while (-not $proc.HasExited) { Start-Sleep -Milliseconds 200 }
+
+    $trackedProperties = @('Id', 'TotalProcessorTime', 'ExitCode', 'HasExited', 'StartTime', 'ExitTime', 'Responding')
+    $data = [ordered]@{}
+    foreach ($prop in $trackedProperties) {
+        $data[$prop] = $proc.$prop
+    }
+    $BuildRequest.ProcessExecutionData = [pscustomobject]$data
+
+    if ($BuildRequest.RedirectStdOut) { $BuildRequest.ProcessExecutionData | Add-Member -MemberType NoteProperty -Name 'StdOutPath' -Value $processParams['RedirectStandardOutput'] }
+    if ($BuildRequest.RedirectStdErr) { $BuildRequest.ProcessExecutionData | Add-Member -MemberType NoteProperty -Name 'StdErrPath' -Value $processParams['RedirectStandardError'] }
+
+    if ($proc.ExitCode -eq 0) {
+        "Build Succeeded (ExitCode: 0)" | Out-BuildTitle
+    } else {
+        "Build Failed (ExitCode: $($proc.ExitCode))" | Out-BuildTitle -Red
+    }
+
+    $roboExe = (get-command "robocopy").Source      
+    if (Test-Path "$OutPath") {
+        Write-Host "Files in OutputPath ($OutPath):" -ForegroundColor Blue
+        Get-ChildItem -Path $OutPath -Recurse | ForEach-Object {
+            $fn = "$($_.FullName)"
+            Write-Host "  $relative" -ForegroundColor White -n
+            Write-Host " copy to `"$RealDeployPath`"" -f DarkCyan
+            
+            $relative = $fn.Substring($OutPath.Length).TrimStart('\', '/')
+            
+        }
+    }
+    &"$roboExe" "$OutPath" "$RealDeployPath" * /E /NFL /NDL /NJH /NJS /NC /NS /NP /R:20 /W:5
+
+}
+
+
+
+
+New-Alias -Name StartBuild -Value Invoke-BuildRequest -Force -ErrorAction Ignore
+
